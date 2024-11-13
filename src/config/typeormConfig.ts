3975fb11a8ebebedd2f81ec1dd7ab11fdbd35fbb
BAKHTIYOR AKHATOV <85681712+Bakhtiyor7@users.noreply.github.com>
2024-11-13 11:46:00 +0900

    fix: typeormconfig -> .env issue

--- src/config/typeormConfig.ts
+++ src/config/typeormConfig.ts
@@ -2,21 +2,15 @@ import { DataSource } from "typeorm";
 import { Upload } from "../entity/Upload";
 import { DataItem } from "../entity/DataItem";
 import * as dotenv from "dotenv";
-dotenv.config();
-
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", process.env.DB_NAME);
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "bahhtee",
-  password: "baxti7877",
-  database: "multisender",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [Upload, DataItem],
   synchronize: true,
 });
